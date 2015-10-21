2b9e9746eddf887a57fb4c8fea3a2f195a49180d
Matthew Trueblood <MattJTrueblood@gmail.com>
2015-10-21 20:27:32 +0900

    Added HTML page and CSS for movement buttons

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Matthew on 10/21/15.
+ */
+
+//Get canvas's element.  Used for drawing the grids and whatnot
+map = document.getElementById("map");
+
+//Get button elements.  Used for moving the map around
+westButton = document.getElementById("westButton");
+eastButton = document.getElementById("eastButton");
+northButton = document.getElementById("northButton");
+southButton = document.getElementById("southButton");
+
