1afc3925432167868b94ecb0e15e17b2cf05f5e7
Matthew Trueblood <MattJTrueblood@gmail.com>
2015-10-21 20:46:54 +0900

    fixed button text selector, added center text display

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,15 @@ eastButton = document.getElementById("eastButton");
 northButton = document.getElementById("northButton");
 southButton = document.getElementById("southButton");
 
+/**
+ * Canvas testing function, for now only used for debug.  Prints an inputted string to the middle of the canvas.
+ * @param stringIn
+ */
+var displayTextInCanvasCenter = function(stringIn) {
+    var context = map.getContext("2d");
+    context.font = "25px Arial";
+    context.textAlign = "center";
+    context.fillText(stringIn, map.width/2, map.height/2);
+}
+
+displayTextInCanvasCenter("Sample Text");
